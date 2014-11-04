cd60669ccc2b422bcf74ce34de1e8641f5148794
Nícolas Iensen <nicolas.iensen@gmail.com>
2014-11-04 21:47:38 -0200

    Add places controller

---
+++ spec/controllers/places_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe PlacesController, :type => :controller do
+  describe "GET index" do
+    context "when there is at least one place" do
+    end
+
+    context "when there is no place" do
+    end
+  end
+end
